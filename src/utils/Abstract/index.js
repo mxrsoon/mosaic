74dc63870c3c227e48510da9d6b0d850864d1724
Marlon Macedo <rm2k3@live.com>
2020-05-07 04:37:07 -0300

    Changed variable declaration in Abstract constructor to const

--- src/utils/Abstract/index.js
+++ src/utils/Abstract/index.js
@@ -1,6 +1,6 @@
 export class Abstract {
     constructor() {
-        let constructor = Object.getPrototypeOf(this).constructor;
+        const constructor = Object.getPrototypeOf(this).constructor;
         
         if (constructor === Abstract || Object.getPrototypeOf(constructor) === Abstract) {
             throw new Error("Abstract classes cannot be instantiated");
