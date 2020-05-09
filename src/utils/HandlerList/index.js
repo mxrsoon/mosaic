005cabc7b3bd384246563f069f68596e12f5e2b8
Marlon Macedo <rm2k3@live.com>
2020-05-09 02:33:46 -0300

    HandlerList#add() and HandlerList#remove() now accept another HandlerList as argument

--- src/utils/HandlerList/index.js
+++ src/utils/HandlerList/index.js
@@ -6,12 +6,20 @@ export class HandlerList {
 	}
 	
 	add(handler) {
+		if (handler instanceof HandlerList) {
+			handler = handler.invoke;
+		}
+
 		if (!this.$.handlers.includes(handler)) {
 			this.$.handlers.push(handler);
 		}
 	}
 	
 	remove(handler) {
+		if (handler instanceof HandlerList) {
+			handler = handler.invoke;
+		}
+		
 		const idx = this.$.handlers.indexOf(handler);
 		
 		if (idx >= 0) {
